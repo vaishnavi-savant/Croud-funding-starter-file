6aceacb23d6fe156a95a69dbfdb27beeb0747b61
vaishnavi-savant <98942134+vaishnavi-savant@users.noreply.github.com>
2023-11-02 21:52:04 +0530

    first

---
+++ Context/contants.js
@@ -0,0 +1,7 @@
+import crowdFunding from "./CrowdFunding.json";
+
+//Crowd Funding MARKETPLACE
+export const CrowdFundingAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const CrowdFundingABI = crowdFunding.abi;
+
+0xF920C89E781cd60308b739eBe820625BfbdD64FB
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,6 @@
+
 const hre = require("hardhat");
+//0x5FbDB2315678afecb367f032d93F642f64180aa3
 
 async function main() {
   const CrowdFunding = await hre.ethers.getContractFactory("CrowdFunding");
@@ -6,8 +8,12 @@ async function main() {
 
   await crowdFunding.deployed();
 
-  console.log(`crowdFunding deployed to ${crowdFunding.address}`);
+  console.log(`CrowdFunding deployed to ${crowdFunding.address}`);
+  
 }
+
+  
+
 
 main().catch((error) => {
   console.error(error);
